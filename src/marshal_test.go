fec2a3d9c8458bcfd1111d8225604411a5faba2b
Dca <dcahsu@gmail.com>
2023-07-27 05:32:47 +0000

    test: use third-party package for verifying message

--- src/marshal_test.go
+++ src/marshal_test.go
@@ -1,25 +1,38 @@
 package msgpack
 
 import (
-	"fmt"
 	"testing"
+	"github.com/stretchr/testify/require"
+	gomsgpack "github.com/shamaton/msgpack/v2"
+
 )
 
 func TestMarshal(t *testing.T) {
 	mp := NewMsgpack()
 
-	jsonObj := map[string]interface{}{
-		"name":    "Dca Hsu",
-		"age":     30,
-		"friends": []string{"someone", "測試"},
+	testCases := []struct {
+		desc string
+		jsonObj map[string]interface{}
+	}{
+		{
+			desc: "Test Case 1",
+			jsonObj: map[string]interface{}{
+				"name":    "Dca Hsu",
+				"age":     30,
+				"friends": []string{"someone", "測試"},
+			},
+		},
 	}
 
-	m, err := mp.Marshal(jsonObj)
-	if err != nil {
-		fmt.Println("Error marshaling to MessagePack:", err)
-		return
-	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			m1, err := mp.Marshal(tc.jsonObj)
+			require.NoError(t, err)
 
-	fmt.Println("MessagePack:", string(m))
-	fmt.Printf("MessagePack: %x\n", m)
+			m2, err := gomsgpack.Marshal(tc.jsonObj)
+			require.NoError(t, err)
+	
+			require.Equal(t, m1, m2, "The two MessagePack byte should be equal")
+		})
+	}
 }
